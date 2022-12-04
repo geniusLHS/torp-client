93fb5b85a51590c53aca2c7601fbd3cbc8ce7a90
geniusLHS <cube040318@gmail.com>
2022-12-04 14:55:15 +0900

    .env 생성, feedbank 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import Header from './components/Header';
-import Link from './components/Link';
-import { io, Socket } from 'socket.io-client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 
 function App() {
+  useEffect(() => {
+    const currentScript = document.querySelector(`script[plugin-key="${process.env.REACT_APP_FEEDBANK_KEY}"]`);
+    if (currentScript) {
+      currentScript.remove();
+      document.querySelector('.fb-plugin')?.remove();
+    }
+    let script = document.createElement('script');
+    script.src = 'https://cdn.feedbank.app/plugin.js';
+    script.defer = true;
+    script.setAttribute('plugin-key', process.env.REACT_APP_FEEDBANK_KEY ?? 'TORP');
+    script.setAttribute('data-fb-button-color', '#e4e4e4');
+    document.head.insertAdjacentElement('beforeend', script);
+  }, []);
+
   return (
     <Router>
       <div className="App font-sans">
